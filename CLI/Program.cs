77a542ad74d20ec7c74324ae2d889c0b72596e35
Periapsises <94928308+Periapsises@users.noreply.github.com>
2025-03-28 21:47:24 -0400

    Add basic test output for transpiler

---
+++ CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var testInput = """
+                        local myVariableDeclaration: number = ( 4 )
+                        """;
+        
+        var transpiler = new Transpiler.Transpiler(testInput);
+        var output = transpiler.Transpile();
+        
+        Console.WriteLine(output);
+    }
+}
