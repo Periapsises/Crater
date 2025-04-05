cd0de62214b9ffae90ff3be709cb061b9585b14d
Periapsises <94928308+Periapsises@users.noreply.github.com>
2025-04-05 15:24:55 -0400

    Highlight string type requirements

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -5,35 +5,12 @@ class Program
     static void Main(string[] args)
     {
         var testInput = """
-                        local myUndefinedNumber: number?
-                        local myDefinedNumber: number = 5
-                        local myDefinedFloat: number = 1.5
-                        local myAddition: number = 1 + 2
-                        local mySubtraction: number = 2 - 1
-                        local myMultiplication: number = 1 * 2
-                        local myDivision: number = 1 / 2
-                        local myExponent: number = 1 ^ 2
-                        local myModulo: number = 1 % 2
-                        local myNegativeNumber: number = -1
-                        local myExponentialNumber: number = 1e2
-                        local myHexadecimalNumber: number = 0xff
-                        local myBinaryNumber: number = 0b01
+                        local myUndefinedString: string?
+                        local myDefinedString: string = "Hi"
+                        local myConcatString: string = "Hello " .. "world!"
                         
-                        local myReferencedNumber: number = myDefinedNumber
-                        local myVariableAddition: number = myDefinedNumber + 1
-                        local myVariableSubtraction: number = myDefinedNumber - 1
-                        local myVariableMultiplication: number = myDefinedNumber * 2
-                        local myVariableDivision: number = myDefinedNumber / 2
-                        local myVariableExponent: number = myDefinedNumber ^ 2
-                        local myVariableModulo: number = myDefinedNumber % 2
-                        local myVariableNegativeNumber: number = myDefinedNumber + -1
-                        
-                        local invalidAddition: number = 1 + "a"
-                        local invalidSubtraction: number = 2 - "a"
-                        local invalidMultiplication: number = 1 * "a"
-                        local invalidDivision: number = 1 / "a"
-                        local invalidExponent: number = 1 ^ "a"
-                        local invalidModulo: number = 1 % "a"
+                        local myReferencedString: string = myDefinedString
+                        local myReferencedConcat: string = myDefinedString .. "?"
                         """;
 
         var maxLineNumberSize = testInput.Split('\n').Length.ToString().Length;
