35f466b07846a4b484c80afd729c03d8c7a912e0
Periapsises <94928308+Periapsises@users.noreply.github.com>
2025-04-09 17:02:27 -0400

    Improve output highlighting

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,15 +1,21 @@
-﻿namespace CLI;
+﻿using System.Diagnostics;
+
+namespace CLI;
 
 class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         var testInput = """
-                        local m: string = "Hello" + 1
-                        
                         if true then
-                            local a: number = 1
+                            local myNestedVar: number = 5
+                            local myOtherNestedVar: number = 5 + myNestedVar
+                            
+                            if myOtherNestedVar == 10 then
+                            end
                         end
+                        
+                        local myVar: number = myNestedVar
                         """;
 
         var maxLineNumberSize = testInput.Split('\n').Length.ToString().Length;
@@ -27,8 +33,12 @@ class Program
 
         Console.WriteLine();
         
+        var stopwatch = Stopwatch.StartNew();
+        
         var transpiler = new Transpiler.Transpiler(testInput);
         var output = transpiler.Transpile();
+        
+        stopwatch.Stop();
 
         foreach (var diagnostic in output.Reporter.ErrorDiagnostics)
             Console.WriteLine(diagnostic.GetMessage());
@@ -46,12 +56,18 @@ class Program
         var errorString = "error" + (numErrors == 1 ? "" : "s");
         var warningString = "warning" + (numWarnings == 1 ? "" : "s");
         var infosString = "info" + (numInfos == 1 ? "" : "s");
+        
+        var errorDisplay = $"\u001b[4m{numErrors}\u001b[24m {errorString}\u001b[0m";
+        if (numErrors != 0) errorDisplay = "\u001b[91m" + errorDisplay;
 
-        var u = "\u001b[4m";
-        var r = "\u001b[0m";
+        var warningDisplay = $"\u001b[4m{numWarnings}\u001b[24m {warningString}\u001b[0m";
+        if (numWarnings != 0) warningDisplay = "\u001b[93m" + warningDisplay;
+        
+        var infoDisplay = $"\u001b[4m{numInfos}\u001b[24m {infosString}\u001b[0m";
+        if (numInfos != 0) infoDisplay = "\u001b[96m" + infoDisplay;
         
         Console.WriteLine();
-        Console.WriteLine($"Transpilation finished with {u}{numErrors}{r} {errorString}, {u}{numWarnings}{r} {warningString} and {u}{numInfos}{r} {infosString}.");
+        Console.WriteLine($"Transpilation finished with {errorDisplay}, {warningDisplay} and {infoDisplay} in {FormatElapsedTime(stopwatch.Elapsed)}.");
         Console.WriteLine();
 
         maxLineNumberSize = output.TranslatedCode.Split('\n').Length.ToString().Length;
@@ -63,5 +79,15 @@ class Program
             var lineNumber = i.ToString().PadLeft(maxLineNumberSize);
             Console.WriteLine($"\u001b[90m{lineNumber}\u001b[0m {lines[i]}");
         }
+    }
+
+    private static string FormatElapsedTime(TimeSpan time)
+    {
+        if (time.TotalSeconds >= 1)
+            return $"{time.TotalSeconds:F2}s";
+        if (time.TotalMilliseconds >= 1)
+            return $"{time.TotalMilliseconds:F0}ms";
+        
+        return $"{time.TotalMicroseconds:F0}µs";
     }
 }
